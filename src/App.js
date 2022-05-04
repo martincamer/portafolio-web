308185f650e378d7600e18d89df9c58384336e52
martincamer011 <martincamer011@gmail.com>
2022-05-04 19:59:20 -0300

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import About from "./components/About";
-import AboutRedes from "./components/AboutRedes";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Navbar from "./components/navbar";
-import Skills from "./components/Skills";
-import SkillsTwo from "./components/SkillsTwo";
-import Work from "./components/Work";
+import About from './components/About';
+import AboutRedes from './components/AboutRedes';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbar from './components/navbar';
+import Skills from './components/Skills';
+import SkillsTwo from './components/SkillsTwo';
+import Work from './components/Work';
 
 function App() {
-  return (
-    <div>
-      <Navbar></Navbar>
-      <Home></Home>
-      <About></About>
-      <Skills></Skills>
-      <SkillsTwo></SkillsTwo>
-      <Work></Work>
-      <Contact></Contact>
-      <AboutRedes></AboutRedes>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar></Navbar>
+			<Home></Home>
+			<About></About>
+			<Skills></Skills>
+			<SkillsTwo></SkillsTwo>
+			{/* <Work></Work> */}
+			<Contact></Contact>
+			<AboutRedes></AboutRedes>
+		</div>
+	);
 }
 
 export default App;
